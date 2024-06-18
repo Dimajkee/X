a65f081ad7a04db0f72d0a503a2c82cedf4afee5
Xiliris <trashyface22@gmail.com>
2024-06-18 16:33:51 +0200

    prvi-projekat

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav id="navbar">
+      <h1>Navbar</h1>
+      <ul>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Projects</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
